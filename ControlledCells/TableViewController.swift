154fabea4406a89833e18130af96f52dc317d3d6
nevillco <connor.neville@raizlabs.com>
2018-04-12 13:11:55 -0400

    Add multiple sections of empty cells

--- ControlledCells/TableViewController.swift
+++ ControlledCells/TableViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Connor Neville. All rights reserved.
 //
 
+import Reusable
 import UIKit
 
 class TableViewController: UITableViewController {
@@ -13,6 +14,26 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Controller-Backed Cells Demo"
+    }
+
+}
+
+extension TableViewController {
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section \(section + 1)"
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 40
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 
 }
